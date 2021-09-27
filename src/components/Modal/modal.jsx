25973911ad2943402680dac210a11d463e3356a7
Guy Sanchez <anakin25@live.com>
2021-09-27 15:52:42 -0600

    :construction: Testing en modal, commit useSpring

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./modal.css";
 import { MdClose } from "react-icons/md";
-import { useSpring, animated } from "react-spring";
+// import { useSpring, animated } from "react-spring";
 
 const Modal = ({ children, isOpen, className = "", onClose }) => {
   const modalRef = React.useRef();
 
-  const animation = useSpring({
-    config: { duration: 250 },
-    opacity: isOpen ? 1 : 0,
-    transform: isOpen ? "translateY(0)" : "translateY(-100%)",
-  });
+  // const animation = useSpring({
+  //   config: { duration: 250 },
+  //   opacity: isOpen ? 1 : 0,
+  //   transform: isOpen ? "translateY(0)" : "translateY(-100%)",
+  // });
 
   const closeModal = (e) => {
     if (modalRef?.current === e?.target) {
@@ -37,14 +37,12 @@ const Modal = ({ children, isOpen, className = "", onClose }) => {
     <React.Fragment>
       {isOpen ? (
         <div className='modal__container' onClick={closeModal} ref={modalRef}>
-          <animated.div style={animation}> 
             <div className={`modal__content`}> 
               <button onClick={() => onClose(prev => !prev)} aria-label='Close Modal' className='modal__close'>
                 <MdClose />
               </button>
               <div className={`modal__main ${className}`}>{children}</div>
             </div>
-          </animated.div>
         </div>
       ) : null}
     </React.Fragment>
