898b4266486c8dc6b013b75d305b08f5debc4dc9
Guy Sanchez <anakin25@live.com>
2021-09-29 16:57:14 -0600

    :construction: Se esta creando view de about me

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles/App.css";
 import Nav from "./View/Navegation/Navegation";
 import Message from "View/Message/message";
-import Modal from "./components/Modal/modal";
+import About from "View/About/About";
 
 function App() {
   const [showModal, setShowModal] = React.useState(false);
@@ -23,14 +23,7 @@ function App() {
           </div>
         </div>
         <main className="main__container">
-          <button onClick={openModal} className='bg-white cursor-pointer py-4 px-4 flex mx-auto'>Open Modal</button>
-          <Modal isOpen={showModal} onClose={setShowModal}>
-            <div className='flex justify-center absolute top-40 right-40'>
-              <h1 className='text-white text-2xl'>Are you ready?</h1>
-              <p className='text-white text-2xl'>Get exclusive access to our next launch.</p>
-              <button className='text-white text-2xl'>Join Now</button>
-            </div>
-          </Modal>
+          <About />
         </main>
       </div>
     </React.Fragment>
---
+++ src/View/About/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './about.css'
+
+const About = () => {
+    return (
+        <React.Fragment>
+            <div className="about__container">
+                <div className="about__container--title">
+                    <h1>About Me</h1>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default About;
